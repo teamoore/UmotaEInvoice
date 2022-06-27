c9df7eed6187d5fee73b16b3ab18bb29b2d4d259
TIMUR.GUNDOGDU <timur.gundogdu@lcwaikiki.com>
2022-06-27 07:09:44 +0300

    service baseresponse eklendi

---
+++ UmotaEFaturaWeb/UmotaEFaturaWeb/Server/Services/Infrastructure/SisKullaniciService.cs
@@ -0,0 +1,13 @@
+﻿using UmotaEFaturaWeb.Shared.ModelDto.Request;
+using UmotaEFaturaWeb.Shared.ModelDto.Response;
+
+namespace UmotaEFaturaWeb.Server.Services.Infrastructure
+{
+    public class SisKullaniciService : ISisKullaniciService
+    {
+        public Task<SisKullaniciLoginResponseDto> Login(SisKullaniciLoginRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UmotaEFaturaWeb/UmotaEFaturaWeb/Shared/ServiceResponses/BaseResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmotaEFaturaWeb.Shared.ServiceResponses
+{
+    public class BaseResponse
+    {
+        public BaseResponse()
+        {
+            Success = true;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public void SetException(Exception ex)
+        {
+            Success = false;
+            Message = ex.Message;
+        }
+    }
+}
